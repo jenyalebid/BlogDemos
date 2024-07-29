14ba33388bf6b4d25ecb29eaecf230530234755c
Jenya Lebid <61124137+jenyalebid@users.noreply.github.com>
2024-07-28 20:14:51 -0700

    Initial Commit

---
+++ BlogDemosTests/BlogDemosTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlogDemosTests.swift
+//  BlogDemosTests
+//
+//  Created by Jenya Lebid on 7/28/24.
+//
+
+import Testing
+@testable import BlogDemos
+
+struct BlogDemosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
